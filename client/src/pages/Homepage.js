65b0ff5a4aa1fd8f0679902eaa2db0d156dde810
zappled <j0el_93@hotmail.com>
2022-11-08 14:30:45 +0800

    Linked 'featured animal' details to mock data

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,21 +2,34 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../common/Navbar";
 import HomepageCarousel from "../components/HomepageCarousel";
 import animals from "../mockData";
+import endangeredIcon from "../images/icons/conservation/endangered.png";
+import criticallyEndangeredIcon from "../images/icons/conservation/critically_endangered.png";
+import vulnerableIcon from "../images/icons/conservation/vulnerable.png";
+import regionIcon from "../images/icons/data/icons8-globe_lightgrey.png";
 
 const Homepage = () => {
   const [animalIndex, setAnimalIndex] = useState(0);
-  const [conservationStatus, setConservationStatus] = useState("");
+  // const [conservationStatus, setConservationStatus] = useState("");
 
   useEffect(() => {
     setAnimalIndex(Math.floor(Math.random() * animals.length));
-    getStatus();
   }, []);
 
+  let conservation_status = "test";
+  let conservationIcon = "";
+
   const getStatus = () => {
-    if (animals[animalIndex].conservation_status === "CR") {
-      setConservationStatus("Critically Endangered");
+    const dataStatus = animals[animalIndex].conservation_status;
+    if (dataStatus === "CR") {
+      conservation_status = "Critically Endangered";
+      conservationIcon = criticallyEndangeredIcon;
+    } else if (dataStatus === "EN") {
+      conservation_status = "Endangered";
+      conservationIcon = endangeredIcon;
     }
   };
+
+  getStatus();
 
   return (
     <>
@@ -30,8 +43,15 @@ const Homepage = () => {
               Animal:
             </div>
             <div className="featured_name">{animals[animalIndex].name}</div>
-            <div className="featured_info">{conservationStatus}</div>
-            <div>{animals[animalIndex].habitat}</div>
+            <div className="featured_info">
+              <img src={conservationIcon} className="featured_icon" />
+              {conservation_status}
+            </div>
+            <div className="featured_info">
+              <img src={regionIcon} className="featured_icon" />
+              {animals[animalIndex].region.join(", ")}
+            </div>
+            <button className="main_button">LEARN MORE</button>
           </div>
           <img className="featured_image" src={animals[animalIndex].image} />
         </div>
